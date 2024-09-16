09be8191cfd265c703aa6ac3572a3276daf7d17e
jakemaier1 <112666479+jakemaier1@users.noreply.github.com>
2024-09-16 14:33:52 -0700

    Add servere.js
    This is the file that initializes a Web Socket Server on my computer, which can then receive the hand location data.

---
+++ VR and AR Project Files/Robot Surp Collab/server.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', ws => {
+    console.log('Client connected');
+
+    ws.on('message', message => {
+        console.log(`Received: ${message}`);
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
